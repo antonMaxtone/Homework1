e2cb113c5519891248b78b1cdc4be5cc7c463338
antonMaxtone <anton.maxtone@gmail.com>
2018-11-09 22:55:52 +0200

    add Tamagochi, without setInterval and randFunc

--- Homework4/Homework4-2/tamagochi.js
+++ Homework4/Homework4-2/tamagochi.js
@@ -7,30 +7,105 @@ function Tamagochi(name) {
 	this.age = 10;
 
 	var ask = confirm("Do you want play with Pikachu?");
-	if (confirm) {
+	if (true) {
 		alert("Open the console and try to play with Pikachu!")
 	} else {
 		alert("Ok! See you next time!")
 	}
 
+
 	this.eat = function () {
-		if (this.happiness == 100 || this.health == 100 && this.boredom == 100) {
+		if (this.happiness == 100 || this.health == 100) {
 			this.happiness += 0;
 			this.health += 0;
-			this.boredom += 0;
 		} else {
 			this.happiness += 10;
 			this.health += 10;
 		}
 
-		console.log(name, "is eating! And now the indicators show us the following results: ");
+		console.log(name, " is ate! And now the indicators show us the following results: ");
 		this.status();
 	}
-	this.sleep = function () {}
-	this.run = function () {}
-	this.play = function () {}
-	this.education = function () {}
-	this.theEnd = function () {}
+
+	this.sleep = function () {
+		if (this.happiness == 100 || this.health == 100 || this.boredom == 100 || this.age == 100) {
+			this.happiness += 0;
+			this.health += 0;
+			this.boredom += 0;
+			this.age += 0;
+		} else {
+			this.happiness += 10;
+			this.health += 10;
+			this.boredom += 10;
+			this.age += 10;
+		}
+		console.log(name, " is slept! And now the indicators show us the following results: ");
+		this.status();
+	}
+
+	this.swim = function () {
+		if (this.happiness == 100 || this.health == 100 || this.sadness == 0) {
+			this.happiness += 0;
+			this.health += 0;
+			this.sadness += 0;
+		} else {
+			this.happiness += 10;
+			this.health += 10;
+			this.sadness -= 10;
+		}
+		console.log(name, "is swam! And now the indicators show us the following results: ");
+		this.status();
+	}
+
+	this.play = function () {
+		if (this.happiness == 100 || this.boredom == 0) {
+			this.happiness += 0;
+			this.boredom += 0;
+		} else {
+			this.happiness += 10;
+			this.boredom -= 10;
+		}
+		console.log(name, "is played! And now the indicators show us the following results: ");
+		this.status();
+	}
+
+	this.education = function () {
+		if (this.age == 100 || this.talent == 100) {
+			this.age += 0;
+			this.talent += 0;
+		} else {
+			this.age += 10;
+			this.talent += 10;
+		}
+		console.log(name, "is learned! And now the indicators show us the following results: ");
+		this.status();
+	}
+	this.fell = function () {
+		if (this.happiness == 0 || this.sadness == 100 || this.health == 0) {
+			this.happiness += 0;
+			this.sadness += 0;
+			this.health += 0;
+		} else {
+			this.happiness -= 10;
+			this.boredom += 10;
+			this.health -= 10;
+		}
+		console.log(name, "is fell! And now the indicators show us the following results: ");
+		this.status();
+	}
+
+	this.accidentallyDisappearedInTheForest = function () {
+		this.happiness = undefined;
+		this.health = undefined;
+		this.talent = undefined;
+		this.sadness = undefined;
+		this.boredom = undefined;
+		this.age = undefined;
+		this.status();
+		console.log("Sorry, but your Pikachu accidentally disappeared in the forest...")
+		// breakTimer		
+	}
+
 	this.status = function () {
 		if (this.happiness === 0 || this.health === 0) {
 			console.log(name, "returns to the egg...")
@@ -42,9 +117,27 @@ function Tamagochi(name) {
 			console.log("Happiness is: " + this.happiness + " ; Health is: " + this.health + " ; Talent is :" + this.talent + " ; Sadness is: " + this.sadness + " ; Boredom is : " + this.boredom + " ; Age is: " + this.age);
 		}
 	}
+	//know it`s not work
+	//	this.arrFunc = [this.accidentallyDisappearedInTheForest, this.fell, this.education, this.play, this.swim, this.sleep, this.eat];
+	//	this.randFunc = () => {
+	//		this.arrFunc[Math.floor(Math.random() * this.arrFunc.length)]();
+	//	}
 }
 
 var toy = new Tamagochi("Pikachu");
 toy.eat();
-toy.eat();
-toy.eat();
+toy.sleep();
+toy.swim();
+toy.play();
+toy.fell();
+toy.education();
+toy.accidentallyDisappearedInTheForest();
+
+//know it`s not work
+
+//var timerId = setInterval(function getLost() {
+//	if (toy.accidentallyDisappearedInTheForest) {
+//		clearInterval(timerId)
+//	}
+//	toy.randFunc()
+//}, 4000)
